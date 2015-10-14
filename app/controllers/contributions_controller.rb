81d8bbe16677020dcc9840b378a03ffec1ecbfbf
Andrew Brennwald & chris underwood <student@epicodus.com>
2015-10-14 14:10:35 -0700

    add the functionality for show, edit, new, and delete method with views

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -9,19 +9,43 @@ class ContributionsController < ApplicationController
     render :show
   end
 
-  # def new
-  # end
-  #
-  # def create
-  # end
-  #
-  # def edit
-  # end
-  #
-  # def update
-  # end
-  #
-  # def destroy
-  # end
+  def new
+    @contribution = Contribution.new
+    render :new
+  end
+
+  def create
+    @contribution = Contribution.new(contribution_params)
+    if @contribution.save
+      redirect_to contributions_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @contribution = Contribution.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @contribution = Contribution.find(params[:id])
+    if @contribution.update(contribution_params)
+      redirect_to contributions_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @contribution = Contribution.find(params[:id])
+    @contribution.destroy
+    redirect_to contributions_path
+  end
+
+  private
+  def contribution_params
+    params.require(:contribution).permit(:name, :content, :rating_total, :rating_count, :subgenre)
+  end
 
 end
